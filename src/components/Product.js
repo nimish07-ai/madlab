946e057fa5a849072a17d953677b2d7c2e1f57ae
nimish07-ai <nimishboda7@gmail.com>
2022-03-31 18:45:21 +0530

    second

--- src/components/Product.js
+++ src/components/Product.js
@@ -21,7 +21,7 @@ export default class Product extends Component {
                   
               <div className = "img-container p-5" onClick = {() => value.handleDetail(id)}>
               <Link to= "/details">
-                  <img src = {`/madlab${img}`} alt="product image" className="card-img-top"/>
+                  <img src = {`/madlab/${img}`} alt="product image" className="card-img-top"/>
               </Link>
               <button className="card-btn" disabled = {inCart?true:false} onClick={ () => {
                 value.addToCart(id);
