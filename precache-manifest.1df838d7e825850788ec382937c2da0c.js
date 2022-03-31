5a051e326887a0d553e429db8059a3649a8bbe09
nimish07-ai <nimishboda7@gmail.com>
2022-03-31 18:46:51 +0530

    Updates

--- precache-manifest.1df838d7e825850788ec382937c2da0c.js
+++ precache-manifest.1df838d7e825850788ec382937c2da0c.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "474cfe1f9d9ae76b686d37fe8df07ef4",
+    "revision": "ab2c91ec40572ae4175e9983f82af13a",
     "url": "/madlab/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/madlab/static/css/2.9be35272.chunk.css"
   },
   {
-    "revision": "bb1ce0f9201adab32350",
+    "revision": "c6dace2296fa2d9c7eee",
     "url": "/madlab/static/css/main.e0e55565.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/madlab/static/js/2.903e3fb3.chunk.js"
   },
   {
-    "revision": "bb1ce0f9201adab32350",
-    "url": "/madlab/static/js/main.7bb77da4.chunk.js"
+    "revision": "c6dace2296fa2d9c7eee",
+    "url": "/madlab/static/js/main.d5ec7830.chunk.js"
   },
   {
     "revision": "396f417b8273cab40f0e",
