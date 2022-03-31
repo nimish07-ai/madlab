e25af4630eeb53faf5e16f4aee17af8a52bdf463
nimish07-ai <nimishboda7@gmail.com>
2022-03-31 18:15:37 +0530

    Updates

---
+++ precache-manifest.30067fec668e6d353883354287ae1e37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de154fb03ba9f4613b27d2ef4e0fbe8f",
+    "url": "/madlab/index.html"
+  },
+  {
+    "revision": "2e71380d3ea5a590fb9a",
+    "url": "/madlab/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "b82375dcbbdcf761fc77",
+    "url": "/madlab/static/css/main.e0e55565.chunk.css"
+  },
+  {
+    "revision": "2e71380d3ea5a590fb9a",
+    "url": "/madlab/static/js/2.fc7aff29.chunk.js"
+  },
+  {
+    "revision": "b82375dcbbdcf761fc77",
+    "url": "/madlab/static/js/main.fdd56bf3.chunk.js"
+  },
+  {
+    "revision": "396f417b8273cab40f0e",
+    "url": "/madlab/static/js/runtime~main.05b0808b.js"
+  },
+  {
+    "revision": "9f7e506d7455da27ce24e474c84a8e0a",
+    "url": "/madlab/static/media/logo.9f7e506d.svg"
+  }
+]);
